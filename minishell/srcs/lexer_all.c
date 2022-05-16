d05e524cafa7119ee708f2520bf593706da8d143
pandamanxv3 <92925204+pandamanxv3@users.noreply.github.com>
2022-05-16 17:24:21 +0200

    Update lexer_all.c

--- minishell/srcs/lexer_all.c
+++ minishell/srcs/lexer_all.c
@@ -14,6 +14,37 @@
 
 extern t_minishell	g_shell;
 
+static int	lexer_redir(int i)
+{
+	int	d;
+
+	d = 0;
+	if ((g_shell.line[i] == '>' && g_shell.line[i + 1] == '<')
+		|| (g_shell.line[i] == '<' && g_shell.line[i + 1] == '>'))
+		return (-1);
+	i++;
+	if (g_shell.line[i] == '>' || g_shell.line[i] == '<')
+		i++;
+	if (g_shell.line[i] == '>' || g_shell.line[i] == '<' || !g_shell.line[i])
+		return (-1);
+	while (g_shell.line[i] == ' ')
+	{
+		i++;
+		if (g_shell.line[i] == '\0')
+			return (-1);
+	}
+	while (g_shell.line[i] && g_shell.line[i] != '>' && g_shell.line[i] != '<'
+		&& g_shell.line[i] != ' ' && g_shell.line[i] != '|')
+	{
+		d++;
+		i++;
+	}
+	puts("LOL");
+	if (d == 0)
+		return (-1);
+	return (i);
+}
+
 static int	lexer_quote(int i)
 {
 	char	c;
@@ -65,6 +96,13 @@ static int	lexer_quote_and_pipe(void)
 	k = 0;
 	while (g_shell.line[i])
 	{
+		if (g_shell.line[i] == '>' || g_shell.line[i] == '<')
+		{
+			i = lexer_redir(i);
+			if (i == -1)
+				return (3);
+			i--;
+		}
 		if (g_shell.line[i] == '"' || g_shell.line[i] == '\'')
 		{
 			i = lexer_quote(i);
@@ -111,12 +149,14 @@ int	lexer_prompt(void)
 	if (lexer_empty_line() == 1)
 		return (1);
 	return_lexer = lexer_quote_and_pipe();
-	if (return_lexer == 1 || return_lexer == 2)
+	if (return_lexer == 1 || return_lexer == 2 || return_lexer == 3)
 	{
 		if (return_lexer == 1)
 			printf("error with the quotes syntax\n");
 		if (return_lexer == 2)
 			printf("error with the pipes syntax\n");
+		if (return_lexer == 3)
+			printf("error with the redirections syntax\n");
 		free(g_shell.line); //on ne free pas g_shell.gc?
 		exit(1);
 	}
